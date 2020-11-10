e20f14c6f0189e3d564a45651a14f585fb73f0c3
sn142 <69063205+sn142@users.noreply.github.com>
2020-11-10 09:33:17 +0800

    winbat

--- softether客户端连接.bat
+++ softether客户端连接.bat
@@ -2,4 +2,6 @@ vpncmd /client localhost /cmd:accountdelete test
 vpncmd /client localhost /cmd:niccreate vpn
 vpncmd /client localhost /cmd:accountcreate test /server:192.168.88.21:5555 /hub:DEFAULT /username:test /nicname:vpn
 vpncmd /client localhost /cmd:accountpasswordset test /password:test /type:standard
+vpncmd /client localhost /cmd:accountencryptdisable test
+vpncmd /client localhost /cmd:accountdetailset test /maxtcp:32 /interval:1 /ttl:0 /half:no /bridge:no /monitor:no /notrack:no /noqos:no
 vpncmd /client localhost /cmd:accountconnect test
